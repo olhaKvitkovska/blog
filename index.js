1b4ce148b0c55e2673e369e87845dfebdee220d8
Olga K <o.kvitkovska@gmail.com>
2019-04-03 23:13:08 +0200

    init repository

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('./app');
+const database = require('./database');
+const config = require('./config');
+
+
+
+database()
+    .then(info => {
+        console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+        console.log(info.name);
+        app.listen(config.PORT, () =>
+            console.log(`Example app listening on port ${config.PORT}!`)
+        );
+    })
+    .catch(() => {
+        console.error('Unable to connect to database');
+        process.exit(1);
+    });
