aace8a14e207cafd36ab7fc4a26baaa3ad3fabd1
Olga K <o.kvitkovska@gmail.com>
2019-04-30 23:54:55 +0200

    Создание Layout и хостинг статики

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+// const Post = require('./models/post');
 
 const app = express();
 
@@ -7,17 +10,40 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/javascripts', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
-const arr = ['hello', 'world', 'test'];
-
-app.get('/', (req, res) => res.render('index', {
-    arr: arr
-}));
-
-app.get('/create', (req, res) => res.render('create'));
-app.post('/create', (req, res) => {
-    arr.push(req.body.text);
-    res.redirect('/');
+app.get('/', (req, res) => {
+    res.render('index');
 });
-
 module.exports = app;
+
+// const arr = ['hello', 'world', 'test'];
+
+// app.get('/', (req, res) => res.render('index', {
+//     arr: arr
+// }));
+
+// app.get('/', (req, res) => {
+//     // Post.find({}).then(posts => {
+//     //     // res.render('index', {
+//     //     //     posts: posts
+//     //     // });
+//     // })
+
+// });
+
+// app.get('/create', (req, res) => res.render('create'));
+// app.post('/create', (req, res) => {
+//     const {
+//         title,
+//         body
+//     } = req.body;
+
+//     Post.create({
+//         title: title,
+//         body: body
+//     }).then(post => console.log(post._id))
+
+//     res.redirect('/');
+// });
